9e2df159f739b7d90abc2633430929e2a5b6854c
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-03-06 01:18:40 -0300

    STYLE: Adição de comentarios

--- src/test/sql/function.sql
+++ src/test/sql/function.sql
@@ -1,10 +1,22 @@
+-- Comandos FUNCTION
+
+
+-- -----------------------------------------------------
+-- Configuração do delimitador
+-- -----------------------------------------------------
 DELIMITER //
 
 
+-- -----------------------------------------------------
+-- Table FUNCTION
+-- -----------------------------------------------------
 DROP FUNCTION IF EXISTS FORMAT//
 CREATE FUNCTION FORMAT(text VARCHAR(255)) RETURNS VARCHAR(255) BEGIN
 	RETURN (UPPER(LTRIM(RTRIM(text))));
 END//
 
 
+-- -----------------------------------------------------
+-- Table ROUTINES
+-- -----------------------------------------------------
 SELECT specific_name FROM information_schema.routines;
